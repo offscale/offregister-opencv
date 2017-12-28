74c5672f05648f6feeac9da0fd6cef376569fa51
Samuel Marks <samuelmarks@gmail.com>
2017-12-29 00:33:46 +1100

    Removed unusued imports; adding repos dir (if not existent)

--- offregister_opencv/base.py
+++ offregister_opencv/base.py
@@ -1,8 +1,8 @@
 from fabric.api import run
-from fabric.context_managers import shell_env, cd
+from fabric.context_managers import cd
 from fabric.operations import sudo
+
 from offregister_fab_utils.apt import apt_depends
-from offregister_fab_utils.fs import cmd_avail
 from offregister_fab_utils.git import clone_or_update
 
 
@@ -10,6 +10,7 @@ def dl_install_opencv(branch='2.4', extra_cmake_args=''):
     apt_depends('build-essential', 'cmake', 'git', 'libgtk2.0-dev', 'pkg-config', 'libavcodec-dev',
                 'libavformat-dev', 'libswscale-dev', 'python-dev', 'python-numpy', 'libtbb2',
                 'libtbb-dev', 'libjpeg-dev', 'libpng-dev', 'libtiff-dev', 'libjasper-dev', 'libdc1394-22-dev')
+    run("mkdir -p '$HOME/repos'", shell_escape=False)
     with cd('$HOME/repos'):
         clone_or_update(repo='opencv', team='opencv', branch=branch, skip_reset=True)
         run('mkdir -p opencv-build')
