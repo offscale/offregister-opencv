fed3a4b20fdb8056e7b043f25472140720ea28fc
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-12-30 23:08:16 -0500

    [setup.py,__init__.py] Standardise install scripts across all off* Python packages; [LICENSE-{APACHE,MIT}] Update copyright year range

--- offregister_opencv/__init__.py
+++ offregister_opencv/__init__.py
@@ -8,6 +8,7 @@ import yaml
 
 __author__ = "Samuel Marks <@SamuelMarks>"
 __version__ = "0.0.5"
+__description__ = "OpenCV deployment module for Fabric (offregister)"
 
 
 def get_logger(name=None):
